1322547fed1b6d9b6f19058b4454441cc72ddfee
hojatm-huma <hojat.modaresizadeh@huma.com>
2026-06-15 21:12:40 +0100

    feat: setup flux using tf

---
+++ helmatform/fluxcd-tf/providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "~> 1.5"
+  required_providers {
+    flux = {
+      source  = "fluxcd/flux"
+      version = "~> 1.8.0"
+    }
+  }
+}
+
+provider "flux" {
+  kubernetes = {
+    config_context = "kind-helmatform"
+    config_path = "~/.kube/config"
+  }
+  git = {
+    url = "https://github.com/hojatm-huma/helmatform.git"
+    http = {
+      username = "git" # This can be any string when using a personal access token
+      password = var.github_token
+    }
+  }
+}
