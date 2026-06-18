7afc47208829f64a087caaf7fe101a2571cd3106
hojatm-huma <hojat.modaresizadeh@huma.com>
2026-06-18 18:34:59 +0100

    feat: add kustomization to tf

--- helmatform/fluxcd-tf/main.tf
+++ helmatform/fluxcd-tf/main.tf
@@ -1,4 +1,6 @@
 resource "flux_bootstrap_git" "this" {
   embedded_manifests = true
   path               = "helmatform/cluster"
+  components_extra = [ "source-watcher" ]
+  kustomization_override = file("${path.root}/kustomization.yaml")
 }
