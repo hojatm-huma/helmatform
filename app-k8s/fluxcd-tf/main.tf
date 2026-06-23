93a571e8c84e3b46811435e8eb46f00cfb56ddad
hojatm-huma <hojat.modaresizadeh@huma.com>
2026-06-23 08:33:45 +0100

    chore: fix flux path

--- app-k8s/fluxcd-tf/main.tf
+++ app-k8s/fluxcd-tf/main.tf
@@ -1,6 +1,6 @@
 resource "flux_bootstrap_git" "this" {
   embedded_manifests = true
-  path               = "app-k8s/flux-system"
+  path               = "app-k8s"
   components_extra = [ "source-watcher" ]
   kustomization_override = file("${path.root}/kustomization.yaml")
 }
